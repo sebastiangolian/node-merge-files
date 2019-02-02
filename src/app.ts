4870d30ba30a7943e836540f1414d39396168b7a
Sebastian Golian <sebastiangolian@gmail.com>
2019-02-02 19:55:45 +0100

    add question and readdir

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,30 @@
 
 console.log("----------- merge-files -----------");
 
-const readline = require('readline');
+import readline from 'readline';
+import fse from 'fs-extra';
 
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
-rl.question("Please enter the path to the file directory (c:\\files): ", (path:string) => {
-    if(path == ""){
+let files:string[] = new Array();
+rl.question("Please enter the path to the file directory (c:\\files): ", (path: string) => {
+    if (path == "") {
         path = "C:\\files"
     }
-    console.log(path);
     rl.close();
+    let files:string[] = fse.readdirSync(path);
+    console.log("1");
+    console.log(files);
+    
+    let ret:string[] = new Array();
+    fse.readdir(path, (err,files) => {
+        if (err) return console.error(err);
+        console.log("2");
+        console.log(files);
+    })
+    
+    
 });
