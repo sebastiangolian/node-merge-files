9099aad756b7186ab39b1d58ff33eb8f10da0c22
Sebastian Golian <sebastiangolian@gmail.com>
2019-02-04 20:45:08 +0100

    add chaining promise

--- src/app.ts
+++ src/app.ts
@@ -1,48 +1,26 @@
 
 console.log("----------- merge-files -----------");
 
-import readline from 'readline';
+import readline from 'readline-promise';
 import fse from 'fs-extra';
-import Promise from "bluebird";
+import { reject } from 'bluebird';
 
-const rl = readline.createInterface({
+const rlp = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
-// let files:string[] = new Array();
-// rl.question("Please enter the path to the file directory (c:\\files): ", (path: string) => {
-//     if (path == "") {
-//         path = "C:\\files"
-//     }
-//     rl.close();
-//     let files:string[] = fse.readdirSync(path);
-//     console.log("1");
-//     console.log(files);
-
-//     let ret:string[] = new Array();
-//     fse.readdir(path, (err,files) => {
-//         if (err) return console.error(err);
-//         console.log("2");
-//         console.log(files);
-//     })
-// });
-
-
-
-async function askQuestion(question:string) {
-    const questionAsync = Promise.promisify<string, string>((question: string, callback: Function) => {
-        rl.question(question,callback.bind(null, null));
-    });
-	return await questionAsync(question);
-}
-
-async function readDir(path:string) {
+rlp.questionAsync("Please enter the path to the file directory (c:\\files): ")
+.then((result:any) => {
+    if(result == ""){
+        return "c:\\files";
+    } else {
+        return result;
+    }
+})
+.then((path:any) => {
     return fse.readdir(path);
-}
-
-askQuestion("Please enter the path to the file directory (c:\\files): ")
-//.then(path => {console.log(path)})
-.then(path => {readDir(path)})
-.then(dirs => {console.log(dirs)})
-.then(() => rl.close());
+})
+.then((files:any) => {
+    console.log(files);
+})
